533f8e180e1cebb87b13b2a1fdc1e458a234d716
Samuel DA MOTA <da.mota.sam@gmail.com>
2016-10-19 22:31:29 +0200

    Bugfix: sanity check was over-paranoid
    In some corner case, it is possible to have say two top skylines
    where one is "included" in the other, having for consequence that
    the top and bottom skyline number would not match anymore.

--- src/svg_extractor.cc
+++ src/svg_extractor.cc
@@ -483,14 +483,6 @@ std::vector<staff_t> get_staves(const pugi::xml_document& svg_file, std::ofstrea
     const auto top_pos = find_top_skyline_pos_of_staff(staves[i], top_skylines);
     const auto bottom_pos = find_bottom_skyline_pos_of_staff(staves[i], bottom_skylines);
 
-    // since top and bottom skylines are sorted, the same way, and for each top
-    // skyline, there is a corresponding bottom skyline, the position of the top
-    // and and bottom skyline of the current staff must match.
-    if (top_pos not_eq bottom_pos)
-    {
-      throw std::runtime_error("Error: the top and bottom skyline for a staff don't match");
-    }
-
     auto top_line = filter_segments(top_skylines[top_pos].full_line,
 				    staves[i].left,
 				    staves[i].right);
